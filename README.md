ff28f20fb9326ef866a976761e1af4dbb1c66652
charleyli <zhenyouli@aliyun.com>
2025-09-11 12:31:13 +0800

    202509111231

--- README.md
+++ README.md
@@ -5,52 +5,6 @@
 ## step 1
 运行一个DeepLabV3+的完整案例，涉及到数据库的处理，模型的搭建、模型的调试、结果的分析
 
-1 文件下载
-https://blog.csdn.net/qq_44961869/article/details/123760704
-https://pan.baidu.com/s/1SgoM8-_JIMJbXyQ9i7eDtw(密码是lala)
-
-
-2 数据处理
-```
-https://mmsegmentation.readthedocs.io/zh-cn/main/user_guides/2_dataset_prepare.html
-```
-最后的数据集的结构如下,见帮助文档：
-mmsegmentation
-├── mmseg
-├── tools
-├── configs
-├── data
-│   ├── potsdam
-│   │   ├── img_dir
-│   │   │   ├── train
-│   │   │   ├── val
-│   │   ├── ann_dir
-│   │   │   ├── train
-│   │   │   ├── val
-ISPRS Potsdam
-Potsdam 数据集是一个有着2D 语义分割内容标注的城市遥感数据集。需要其中的 ‘2_Ortho_RGB.zip’ 和 ‘5_Labels_all_noBoundary.zip’。
-对于 Potsdam 数据集，请运行以下命令使用我们默认的配置， 将生成 3456 张图片的训练集和 2016 张图片的验证集。
- 使用这个命令：
- ```python
- python tools/dataset_converters/potsdam.py  /data/potsdam
- ```
- 数据在C:\data\potsdam文件夹下。
-在05_Labels_all_noBoundary.py中，验证C:\data\potsdam\5_Labels_all_noBoundary\top_potsdam_2_10_label_noBoundary.tif有多少颜色类别。结果如下：
-```
-# 结果为：
-# 标注图里的颜色值：
-# 类别 0: [0, 0, 0]
-# 类别 1: [0, 0, 255]
-# 类别 2: [0, 255, 0]
-# 类别 3: [0, 255, 255]
-# 类别 4: [255, 0, 0]
-# 类别 5: [255, 255, 0]
-# 类别 6: [255, 255, 255]
-```
-我们将模块化和继承性设计融入到我们的配置文件系统中，方便进行各种实验。如果您想查看配置文件，你可以运行 python tools/misc/print_config.py /PATH/TO/CONFIG 来查看完整的配置文件。
-
-
-
 
 
 ## step 2
--- tools/execute.py
+++ tools/execute.py
@@ -1,37 +1,42 @@
 import os
+import torch
 import numpy as np
-import cv2
-from mmseg.apis import init_model, inference_model, show_result_pyplot
-import mmcv
 import matplotlib.pyplot as plt
+import mmcv
+from mmseg.apis import init_model, inference_model, show_result_pyplot
+from mmseg.utils import register_all_modules
+from PIL import Image
+from mmseg.apis import init_model
+from mmseg.apis import inference_model
+from mmengine.model.utils import revert_sync_batchnorm
+from mmseg.apis import show_result_pyplot
 
 
-# 创建 checkpoint 文件夹，用于存放预训练模型权重文件
-# 创建 outputs 文件夹，用于存放预测结果
-# 创建 data 文件夹，用于存放图片和视频素材
-# 创建 图表 文件夹，用于存放生成的图表
-# 创建 Zihao-Configs 文件夹，用于存放自己的语义分割模型的 config 配置文件
-folders = ['checkpoint', 'outputs', 'data', '图表', 'Zihao-Configs']
-for folder in folders:
-    os.makedirs(folder, exist_ok=True)  # 如果已存在就不报错
+print("cwd:", os.getcwd())
+config_file = './configs/pspnet/pspnet_r50-d8_4xb2-40k_cityscapes-512x1024.py'
+checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/pspnet/pspnet_r50-d8_512x1024_40k_cityscapes/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth'
+register_all_modules()
+model = init_model(config_file, checkpoint_file, device='cuda:0')
+if not torch.cuda.is_available():
+    model = revert_sync_batchnorm(model)
 
+img_path = './demo/demo.png'
+img_pil = Image.open(img_path)
+print("img_pil.size",img_pil.size)
+result = inference_model(model, img_path)
+print("result",result)
+keys = result.keys()
+print(keys)
+print("pred_sem_seg.shape:", result.pred_sem_seg.data.shape)
+print("np.unique(result.pred_sem_seg.data.cpu())",np.unique(result.pred_sem_seg.data.cpu()))
 
-
-
-
-
-# 模型 config 配置文件
-config_file = 'configs/segformer/segformer_mit-b5_8xb1-160k_cityscapes-1024x1024.py'
-
-# 模型 checkpoint 权重文件
-checkpoint_file = 'https://download.openmmlab.com/mmsegmentation/v0.5/segformer/segformer_mit-b5_8x1_1024x1024_160k_cityscapes/segformer_mit-b5_8x1_1024x1024_160k_cityscapes_20211206_072934-87a052ec.pth'
-
-img_path = 'data/street_uk.jpeg'
-
-# img_bgr = cv2.imread(img_path)
-
-# print("img_bgr.shape",img_bgr.shape)
-# plt.imshow(img_bgr[:,:,::-1])
-# plt.show()
-
-# result = inference_model(model, img_bgr)
+print("seg_logits.shape:", result.seg_logits.data.shape)
+class_map = result.pred_sem_seg.data[0].detach().cpu().numpy()
+plt.imshow(class_map)
+plt.show()
+visualization = show_result_pyplot(model, img_path, result, opacity=0.8, title='MMSeg', out_file='./outputs/B2.jpg')
+print(visualization.shape)
+visualization.shape
+plt.imshow(mmcv.bgr2rgb(visualization))
+plt.show()
+Image.open('./outputs/B2.jpg')
